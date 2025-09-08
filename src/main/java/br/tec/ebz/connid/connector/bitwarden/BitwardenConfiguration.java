0089d26f53f457c3ede3f1b0419e3c3d36ee225b
Jean M Santos <jean.michel@ebz.tec.br>
2025-09-08 15:13:22 -0300

    added Bitwarden connector configuration and configurations tests

--- src/main/java/br/tec/ebz/connid/connector/bitwarden/BitwardenConfiguration.java
+++ src/main/java/br/tec/ebz/connid/connector/bitwarden/BitwardenConfiguration.java
@@ -16,28 +16,86 @@
 
 package br.tec.ebz.connid.connector.bitwarden;
 
+import org.identityconnectors.common.security.GuardedString;
+import org.identityconnectors.framework.common.exceptions.ConfigurationException;
 import org.identityconnectors.framework.spi.AbstractConfiguration;
 import org.identityconnectors.common.logging.Log;
 import org.identityconnectors.framework.spi.ConfigurationProperty;
+
+import java.net.URI;
+import java.net.URL;
 
 public class BitwardenConfiguration extends AbstractConfiguration {
 
     private static final Log LOG = Log.getLog(BitwardenConfiguration.class);
 
-    private String sampleProperty;
+    private String hostUrl;
+    private String clientId;
+    private GuardedString clientSecret;
+
+    @ConfigurationProperty(
+            displayMessageKey = "bitwarden.config.hostUrl" ,
+            helpMessageKey = "bitwarden.config.hostUrl.help",
+            required = true,
+            order = 1
+    )
+    public String getHostUrl() {
+        return hostUrl;
+    }
+
+    public void setHostUrl(String hostUrl) {
+        this.hostUrl = hostUrl;
+    }
+
+    @ConfigurationProperty(
+            displayMessageKey = "bitwarden.config.clientId" ,
+            helpMessageKey = "bitwarden.config.clientId.help",
+            required = true,
+            order = 2
+    )
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    @ConfigurationProperty(
+            displayMessageKey = "bitwarden.config.clientSecret" ,
+            helpMessageKey = "bitwarden.config.clientSecret.help",
+            required = true,
+            order = 3,
+            confidential = true
+    )
+    public GuardedString getClientSecret() {
+        return clientSecret;
+    }
+
+    public void setClientSecret(GuardedString clientSecret) {
+        this.clientSecret = clientSecret;
+    }
 
     @Override
     public void validate() {
-        //todo implement
-    }
+        String errorMessage = null;
 
-    @ConfigurationProperty(displayMessageKey = "bitwarden.config.sampleProperty",
-            helpMessageKey = "bitwarden.config.sampleProperty.help")
-    public String getSampleProperty() {
-        return sampleProperty;
-    }
+        if (hostUrl == null || hostUrl.isBlank()) {
+            errorMessage = "Host url is required.";
+        } else if (clientId == null || clientId.isBlank()) {
+            errorMessage = "Client ID is required.";
+        } else if (clientSecret == null) {
+            errorMessage = "Client secret is required.";
+        } else {
+            try {
+                new URL(hostUrl).toURI();
+            } catch (Exception e) {
+                errorMessage = "Invalid URL. Please, provide a valid URL.";
+            }
+        }
 
-    public void setSampleProperty(String sampleProperty) {
-        this.sampleProperty = sampleProperty;
+        if (errorMessage != null) {
+            throw new ConfigurationException(errorMessage);
+        }
     }
 }
--- src/test/java/br/tec/ebz/connid/connector/bitwarden/BitwardenConfigurationTest.java
+++ src/test/java/br/tec/ebz/connid/connector/bitwarden/BitwardenConfigurationTest.java
@@ -1,5 +1,7 @@
 package br.tec.ebz.connid.connector.bitwarden;
 
+import org.identityconnectors.common.security.GuardedString;
+import org.identityconnectors.framework.common.exceptions.ConfigurationException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,8 +9,76 @@ import static org.junit.jupiter.api.Assertions.*;
 class BitwardenConfigurationTest {
 
     @Test
-    void validate() {
-        BitwardenConfiguration bitwardenConfiguration = new BitwardenConfiguration();
-        bitwardenConfiguration.validate();
+    void validateValidConfigurationInputs() {
+        BitwardenConfiguration configuration = new BitwardenConfiguration();
+
+        configuration.setHostUrl("https://iam.validurl.com");
+        configuration.setClientId("I_AM_A_CLIENTID");
+        configuration.setClientSecret(new GuardedString("i_am_client_secret".toCharArray()));
+
+        configuration.validate();
+
+        assertEquals("https://iam.validurl.com", configuration.getHostUrl());
+        assertEquals("I_AM_A_CLIENTID", configuration.getClientId());
+        assertNotNull(configuration.getClientSecret());
     }
+
+    @Test
+    void validateNullHostConfiguration() {
+        BitwardenConfiguration configuration = new BitwardenConfiguration();
+        configuration.setClientId("I_AM_A_CLIENTID");
+        configuration.setClientSecret(new GuardedString("i_am_client_secret".toCharArray()));
+
+        assertThrows(ConfigurationException.class, configuration::validate);
+    }
+
+    @Test
+    void validateEmptyHostConfiguration() {
+        BitwardenConfiguration configuration = new BitwardenConfiguration();
+        configuration.setHostUrl(" ");
+        configuration.setClientId("I_AM_A_CLIENTID");
+        configuration.setClientSecret(new GuardedString("i_am_client_secret".toCharArray()));
+
+        assertThrows(ConfigurationException.class, configuration::validate);
+    }
+
+    @Test
+    void validateNullClientIdConfiguration() {
+        BitwardenConfiguration configuration = new BitwardenConfiguration();
+        configuration.setHostUrl("https://iam.validurl.com");
+        configuration.setClientSecret(new GuardedString("i_am_client_secret".toCharArray()));
+
+        assertThrows(ConfigurationException.class, configuration::validate);
+    }
+
+    @Test
+    void validateEmptyClientIdConfiguration() {
+        BitwardenConfiguration configuration = new BitwardenConfiguration();
+        configuration.setHostUrl("https://iam.validurl.com");
+        configuration.setClientId("");
+        configuration.setClientSecret(new GuardedString("i_am_client_secret".toCharArray()));
+
+        assertThrows(ConfigurationException.class, configuration::validate);
+    }
+
+    @Test
+    void validateEmptyClientSecretConfiguration() {
+        BitwardenConfiguration configuration = new BitwardenConfiguration();
+        configuration.setHostUrl("https://iam.validurl.com");
+        configuration.setClientId("I_AM_A_CLIENT_ID");
+
+        assertThrows(ConfigurationException.class, configuration::validate);
+    }
+
+    @Test
+    void validInvalidHostUrlConfiguration() {
+        BitwardenConfiguration configuration = new BitwardenConfiguration();
+
+        configuration.setHostUrl("iam.validurl.com");
+        configuration.setClientId("I_AM_A_CLIENTID");
+        configuration.setClientSecret(new GuardedString("i_am_client_secret".toCharArray()));
+
+        assertThrows(ConfigurationException.class, configuration::validate);
+    }
+
 }
