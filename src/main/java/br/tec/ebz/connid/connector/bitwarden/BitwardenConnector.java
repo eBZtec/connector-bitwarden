b7c01b45ba24e2ca53a96ccef30360d616a12612
Jean M Santos <jean.michel@ebz.tec.br>
2025-09-08 14:03:16 -0300

    first commit

---
+++ src/main/java/br/tec/ebz/connid/connector/bitwarden/BitwardenConnector.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2010-2014 Evolveum
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package br.tec.ebz.connid.connector.bitwarden;
+
+import org.identityconnectors.common.logging.Log;
+import org.identityconnectors.framework.spi.Configuration;
+import org.identityconnectors.framework.spi.Connector;
+import org.identityconnectors.framework.spi.ConnectorClass;
+
+@ConnectorClass(displayNameKey = "bitwarden.connector.display", configurationClass = BitwardenConfiguration.class)
+public class BitwardenConnector implements Connector {
+
+    private static final Log LOG = Log.getLog(BitwardenConnector.class);
+
+    private BitwardenConfiguration configuration;
+    private BitwardenConnection connection;
+
+    @Override
+    public Configuration getConfiguration() {
+        return configuration;
+    }
+
+    @Override
+    public void init(Configuration configuration) {
+        this.configuration = (BitwardenConfiguration)configuration;
+        this.connection = new BitwardenConnection(this.configuration);
+    }
+
+    @Override
+    public void dispose() {
+        configuration = null;
+        if (connection != null) {
+            connection.dispose();
+            connection = null;
+        }
+    }
+}
