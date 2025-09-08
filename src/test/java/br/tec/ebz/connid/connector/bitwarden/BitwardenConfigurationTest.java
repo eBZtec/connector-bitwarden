a483f1cbe586a552d8bd6a7fe3d1bc50d79baf60
Jean M Santos <jean.michel@ebz.tec.br>
2025-09-08 14:34:07 -0300

    junit5 and jacoco configuration

---
+++ src/test/java/br/tec/ebz/connid/connector/bitwarden/BitwardenConfigurationTest.java
@@ -0,0 +1,12 @@
+package br.tec.ebz.connid.connector.bitwarden;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BitwardenConfigurationTest {
+
+    @Test
+    void validate() {
+    }
+}
